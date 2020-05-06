6f8ce3d551c60f6c5574c054cb108fc4a2dac08a
hyeonjuice0328 <jjuccee@gmail.com>
2020-05-06 17:53:37 +0900

    학원 커밋 practice 2020-05-06

---
+++ Jungol_기초다지기/src/반복제어문2/형성평가01/Main.java
@@ -0,0 +1,13 @@
+package 반복제어문2.형성평가01;
+import java.util.Scanner;
+
+//10 이하의 자연수 n을 입력받아 "JUNGOL​"을 n번 출력하는 프로그램을 작성하시오.
+
+public class Main {
+	
+	Scanner sc = new Scanner(System.in);
+	int n = sc.nextInt();
+	
+	
+	
+}
