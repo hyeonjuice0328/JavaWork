341a6995b2ae7a8eb3ee357fc2e61abee9f6b01a
hyeonjuice0328 <jjuccee@gmail.com>
2020-05-08 12:34:34 +0900

    학원 커밋(오전수업) 2020-05-08

---
+++ Lec_WEB/WebContent/03_JavaScript/JS/myScript.js
@@ -0,0 +1,16 @@
+/**
+ * TODO
+ */
+
+ function light(sw){
+     var pic;
+     if( sw == 0){
+        pic = "https://www.w3schools.com/html/pic_bulboff.gif"
+     } else {
+        pic = "https://www.w3schools.com/html/pic_bulbon.gif"
+     }
+     document.getElementById('myImage').src = pic;
+     // Attibute 변경 방법
+     // document.Element.Attribute 를 넣어준다.
+     // <img> 의 src 값 변경 
+ }
